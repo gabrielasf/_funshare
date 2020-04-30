3a328f67f1440c3e090286bd784d5f7784d821b3
gabrielasf <gabfloriani@gmail.com>
2020-04-30 11:35:05 +0200

    cont login

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -43,7 +43,7 @@ import { withRouter } from 'react-router';
     render() {
       const { username, password, message } = this.state;
       return (
-        <div class="container-fluid">
+        <div class="contlogin container-fluid">
   <div className="row no-gutter">
     <div className="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
     <div className="col-md-8 col-lg-6">

f9189f5efb6c537312644598de27798d68f4922c
DonatellaC <donatella.carta@mailbox.org>
2020-05-02 13:47:55 +0200

    Event: css with 3 columns

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -226,7 +226,7 @@ export default class Event extends Component {
                   {this.state.events.map((event, index) => {
                     return (
                       <div key={index}>
-                        <div class="test">
+                        <div class="test shadow rounded border">
                           <div class="card-image-test"></div>
                           <div class="card-text-test">
                             <span className="label"></span>
@@ -237,7 +237,29 @@ export default class Event extends Component {
                               <h4>{event.location}</h4>
                             </span>
                             <span className="label"> </span>
-                            {event.email}
+
+                            <a href={`mailto:${event.email}`}>
+                              <svg
+                                class="bi bi-envelope"
+                                width="2em"
+                                height="2em"
+                                viewBox="0 0 16 16"
+                                fill="currentColor"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  fill-rule="evenodd"
+                                  d="M14 3H2a1 1 0 00-1 1v8a1 1 0 001 1h12a1 1 0 001-1V4a1 1 0 00-1-1zM2 2a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V4a2 2 0 00-2-2H2z"
+                                  clip-rule="evenodd"
+                                />
+                                <path
+                                  fill-rule="evenodd"
+                                  d="M.071 4.243a.5.5 0 01.686-.172L8 8.417l7.243-4.346a.5.5 0 01.514.858L8 9.583.243 4.93a.5.5 0 01-.172-.686z"
+                                  clip-rule="evenodd"
+                                />
+                                <path d="M6.752 8.932l.432-.252-.504-.864-.432.252.504.864zm-6 3.5l6-3.5-.504-.864-6 3.5.504.864zm8.496-3.5l-.432-.252.504-.864.432.252-.504.864zm6 3.5l-6-3.5.504-.864 6 3.5-.504.864z" />
+                              </svg>
+                            </a>
                           </div>
                           <div class="card-stats">
                             <div class="stat border">
